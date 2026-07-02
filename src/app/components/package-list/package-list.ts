3ec854aaaf9c6f6959ec2512981ed90802250d70
Arevik <arevik.charchyan@gmail.com>
2026-07-02 12:23:44 +0400

    feat: searchbar, filtration, layout fix

--- src/app/components/package-list/package-list.ts
+++ src/app/components/package-list/package-list.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { PackageApiService } from '../../services/package-api.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { Package } from '../../models/package.model';
 import { PackageCard } from '../package-card/package-card';
 
@@ -14,5 +14,16 @@ import { PackageCard } from '../package-card/package-card';
 })
 export class PackageList {
   private api = inject(PackageApiService);
-  packages$: Observable<Package[]> = this.api.getPackages();
+  private packages$: Observable<Package[]> = this.api.getPackages();
+  private filterText$ = new BehaviorSubject<string>('');
+
+  filteredPackages$: Observable<Package[]> = combineLatest([this.packages$, this.filterText$]).pipe(
+    map(([packages, filterText]: [Package[], string]) =>
+      packages.filter((p: Package) => p.id.toLowerCase().includes(filterText.toLowerCase())),
+    ),
+  );
+
+  onFilterChange(value: string): void {
+    this.filterText$.next(value);
+  }
 }
