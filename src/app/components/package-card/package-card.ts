20be43fc32185936dcf3ce2e8c3418d8b6091849
Arevik <arevik.charchyan@gmail.com>
2026-07-02 12:01:03 +0400

    chore: package card style and title color splitting

--- src/app/components/package-card/package-card.ts
+++ src/app/components/package-card/package-card.ts
@@ -10,4 +10,11 @@ import { Package } from '../../models/package.model';
 })
 export class PackageCard {
   @Input() package!: Package;
+
+  get idParts(): { scope: string | null; name: string } {
+    const i = this.package.id.indexOf('/');
+    return i === -1
+      ? { scope: null, name: this.package.id }
+      : { scope: this.package.id.slice(0, i), name: this.package.id.slice(i + 1) };
+  }
 }
